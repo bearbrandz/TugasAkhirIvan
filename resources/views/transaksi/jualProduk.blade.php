bb52ea2f70bf46daefa9f9f1b6aa069a6cb79208
Mr Panda <149362092+bearbrandz@users.noreply.github.com>
2026-06-09 11:22:12 +0700

    Fix UI: Remove redundant success and error alerts

--- resources/views/transaksi/jualProduk.blade.php
+++ resources/views/transaksi/jualProduk.blade.php
@@ -148,23 +148,6 @@
     </style>
 
     <div class="container-fluid">
-        @if (session('success'))
-            <div class="alert alert-success">
-                {{ session('success') }}
-            </div>
-        @endif
-
-        @if (session('status'))
-            <div class="alert alert-success">
-                {{ session('status') }}
-            </div>
-        @endif
-
-        @if (session('error'))
-            <div class="alert alert-danger">
-                {{ session('error') }}
-            </div>
-        @endif
 
         @if ($errors->any())
             <div class="alert alert-danger">
